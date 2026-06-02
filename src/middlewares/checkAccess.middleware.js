203a56583a8d22b20bde3c6ee461a6031ae0c92b
Maks <m.o.kosianchuk@strudent.khai>
2026-06-02 17:42:59 +0300

    Added edit post option

--- src/middlewares/checkAccess.middleware.js
+++ src/middlewares/checkAccess.middleware.js
@@ -1,9 +1,9 @@
 const ForbiddenError = require("../errors/ForbiddenError")
 
-const checkAccessMiddleware = async (req, res, next) => {
+const checkAdminAccessMiddleware = async (req, res, next) => {
     try {
         if(!req.profile || !req.profile.is_admin) {
-            throw new ForbiddenError({ message: "This user doesn't have permission to create a post!" })
+            throw new ForbiddenError({ message: "This user doesn't have permission to do this action!" })
         }
 
         next()
@@ -13,4 +13,4 @@ const checkAccessMiddleware = async (req, res, next) => {
     }
 }
 
-module.exports = checkAccessMiddleware
+module.exports = checkAdminAccessMiddleware
